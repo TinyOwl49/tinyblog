41b49a8c8c862616a0984b645fbaf7acd5bc6ff9
Tiny Owl <102298238+TinyOwl49@users.noreply.github.com>
2025-03-12 06:32:12 +0900

    fix ogp settings

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,4 +4,16 @@
 	import { articles } from "$lib/stores";
 </script>
 
+<svelte:head>
+	<meta property="og:title" content={`Tiny Forest`} />
+	<meta property="og:type" content="article" />
+	<!-- <meta property="og:url" content=${} /> -->
+	<meta property="og:site_name" content="Tiny Forest" />
+	<meta
+		property="og:description"
+		content="ちょっとしたことを書いていく個人ブログです。"
+	/>
+	<!-- WARNING: あまりにひどい方法なのでいつか直す -->
+	<meta property="og:image" content="https://raw.githubusercontent.com/TinyOwl49/tinyblog/refs/heads/main/static/favicon.png" />
+</svelte:head>
 <ArticleList articles={$articles} />
--- src/routes/article/[slug]/+page.svelte
+++ src/routes/article/[slug]/+page.svelte
@@ -14,7 +14,8 @@
 	<!-- <meta property="og:url" content=${} /> -->
 	<meta property="og:site_name" content="Tiny Forest" />
 	<meta property="og:description" content={data.description} />
-	<!--<meta property="og:image" content="{base}/static/favicon.png" />-->
+	<!-- WARNING: あまりにひどい方法なのでいつか直す -->
+	<meta property="og:image" content="https://raw.githubusercontent.com/TinyOwl49/tinyblog/refs/heads/main/static/favicon.png" />
 </svelte:head>
 
 <div>
